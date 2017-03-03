268739b8a728c20f835872a683b108cd8babc7e1
schwindelig <david.szoeke@gmail.com>
2017-03-03 08:09:19 +0100

    Solution setup

---
+++ microsoft-70-483/src/Snippets.Microsoft70483.MultithreadingAndAsync/TPL/ParallelFor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Snippets.Microsoft70483.Shared;
+
+namespace Snippets.Microsoft70483.MultithreadingAndAsync.TPL
+{
+    public class ParallelFor : IRunnable
+    {
+        public void Run(params object[] args)
+        {
+
+        }
+    }
+}
---
+++ microsoft-70-483/src/Snippets.Microsoft70483.Shared/IRunnable.cs
@@ -0,0 +1,7 @@
+﻿namespace Snippets.Microsoft70483.Shared
+{
+    public interface IRunnable
+    {
+        void Run(params object[] args);
+    }
+}
